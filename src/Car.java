dbdd35140c8f0bda9e79b1cef3579cd53fc7abfd
smvconnect <smvconnect@gmail.com>
2022-01-12 09:32:20 +0000

    first commit

---
+++ src/Car.java
@@ -0,0 +1,5 @@
+public class Car {
+    public void build() {
+        System.out.println("You must build a new car");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] argv) {
+        Car newCar = new Car();
+        newCar.build();
+    }
+}
